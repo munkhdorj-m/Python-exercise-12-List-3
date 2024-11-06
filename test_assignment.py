0f985ba6cbe47961403bc13fb77054d5723a244e
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-11-06 23:38:41 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -1,41 +1,32 @@
 import pytest
 import inspect
-from assignment import cube_elements, remove_duplicates, reverse_list
+from assignment import is_palindrome, is_strictly_ascending
 
 def check_contains_loop(function):
     source = inspect.getsource(function)
     return 'for' in source or 'while' in source
 
-@pytest.mark.parametrize("numbers, expected", [
-    ([1, 2, 3], [1, 8, 27]),
-    ([0, 1, 2], [0, 1, 8]),
-    ([2, 3, 4], [8, 27, 64]),
-    ([-1, -2, -3], [-1, -8, -27]),
-    ([2], [8])
+# Test cases for is_palindrome
+@pytest.mark.parametrize("lst, expected", [
+    ([1, 2, 3, 2, 1], True),
+    ([1, 2, 3, 4, 5], False),
+    ([7, 8, 8, 7], True),
+    ([1, 1, 1, 1], True),
+    ([], True)
 ])
-def test1(numbers, expected):
-    assert cube_elements(numbers) == expected
-    assert check_contains_loop(cube_elements)
+def test1(lst, expected):
+    assert is_palindrome(lst) == expected
+    assert check_contains_loop(is_palindrome)
 
-@pytest.mark.parametrize("numbers, expected", [
-    ([1, 2, 2, 3, 4, 4], [1, 2, 3, 4]),
-    ([5, 5, 5, 5], [5]),
-    ([1, 2, 3], [1, 2, 3]),
-    ([1, 2, 2, 3, 1, 4], [1, 2, 3, 4]),
-    ([0, 0, 0], [0])
+# Test cases for is_strictly_ascending
+@pytest.mark.parametrize("lst, expected", [
+    ([1, 2, 3, 4, 5], True),
+    ([-5, 10, 99, 123456] , True),
+    ([1, 1, 2, 3, 4], False),
+    ([4, 5, 6, 7, 3, 7, 9] , True),
+    ([2, 3, 3, 4, 5]  , False),
+    ([10], True)
 ])
-def test2(numbers, expected):
-    assert remove_duplicates(numbers) == expected
-    assert check_contains_loop(remove_duplicates)
-
-@pytest.mark.parametrize("numbers, expected", [
-    ([1, 2, 3], [3, 2, 1]),
-    ([4, 5, 6, 7], [7, 6, 5, 4]),
-    ([1], [1]),
-    ([], []),
-    ([10, 20, 30], [30, 20, 10])
-])
-def test3(numbers, expected):
-    assert reverse_list(numbers) == expected
-    assert check_contains_loop(reverse_list)
-
+def test2(lst, expected):
+    assert is_strictly_ascending(lst) == expected
+    assert check_contains_loop(is_strictly_ascending)
